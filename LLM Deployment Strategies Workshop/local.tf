f5c973d1ff6fd5fa8bfd64b1d58ab32b7e6527c2
rostmyronenko <rimironenko@gmail.com>
2025-07-03 19:33:51 +0200

    Fixed ollama on AWS EC2

--- LLM Deployment Strategies Workshop/local.tf
+++ LLM Deployment Strategies Workshop/local.tf
@@ -1,27 +1,17 @@
 locals {
   vpc_cidr  = "10.0.0.0/16"
-  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs       = slice(data.aws_availability_zones.available.names, 0, 3)
   user_data = <<-EOT
               #!/bin/bash
               sudo yum update -y
               sudo yum install -y gcc kernel-devel-$(uname -r)
-              aws s3 cp --recursive s3://ec2-linux-nvidia-drivers/latest/ .
-              chmod +x NVIDIA-Linux-x86_64*.run
               mkdir /home/ec2-user/tmp
               chmod -R 777 tmp
               export TMPDIR=/home/ec2-user/tmp
-              CC=/usr/bin/gcc10-cc ./NVIDIA-Linux-x86_64*.run --tmpdir=$TMPDIR
-              sudo touch /etc/modprobe.d/nvidia.conf
-              echo "options nvidia NVreg_EnableGpuFirmware=0" | sudo tee --append /etc/modprobe.d/nvidia.conf
               sudo yum install -y docker git
               sudo usermod -a -G docker ec2-user
               sudo systemctl enable docker.service
               sudo systemctl start docker.service
-              curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | \
-                sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
-              sudo yum install -y nvidia-container-toolkit
-              sudo nvidia-ctk runtime configure --runtime=docker
-              sudo systemctl restart docker
               sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
               sudo chmod +x /usr/local/bin/docker-compose
               docker-compose version
--- LLM Deployment Strategies Workshop/main.tf
+++ LLM Deployment Strategies Workshop/main.tf
@@ -1,6 +1,6 @@
 module "ollama_ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.7.1"
+  version = "6.0.2"
 
   name          = "ollama-${random_pet.this.id}"
   ami           = data.aws_ami.amazon_linux.id
@@ -8,7 +8,7 @@ module "ollama_ec2_instance" {
 
   availability_zone           = module.vpc.azs[0]
   subnet_id                   = module.vpc.public_subnets[0]
-  vpc_security_group_ids = [module.security_group.security_group_id]
+  vpc_security_group_ids      = [module.security_group.security_group_id]
   create_eip                  = true
   associate_public_ip_address = true
 
@@ -18,16 +18,15 @@ module "ollama_ec2_instance" {
     AdministratorAccess = "arn:aws:iam::aws:policy/AdministratorAccess"
   }
 
-  root_block_device = [
-    {
-      volume_size = 100
-      volume_type = "gp3"
-    }
-  ]
+  root_block_device = {
+    size = 100
+    type = "gp3"
+  }
+
 
   tags = var.aws_resource_tags
 
-  user_data_base64 = base64encode(local.user_data)
+  user_data_base64            = base64encode(local.user_data)
   user_data_replace_on_change = true
 
 }
